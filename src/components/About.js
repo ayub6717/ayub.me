a1931e9c7f8d1c7e537f221486a243bfeef7d3d0
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-08 02:37:33 +0600

    m

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ return (
     <div className="about">
       <div className="about-details">
         <p>
-          Hello! I'm Md. Ayub Shamim, a passionate Web developer. I develop websites, web applications. My core skill is
+          Hello! I'm Md. Ayub, a passionate Web developer. I develop websites, web applications. My core skill is
           based on JavaScript and I love to do most things using JavaScript. I love to make the web more open to the
           world. I have familiar with a wide range of data structures, algorithms, Object-oriented programming, and
           databases. I have graduated with a bachelor's degree in Computer Science Engineering from Daffodil
